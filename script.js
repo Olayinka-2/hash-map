b8324cf6e150b41cc28affe23dcd8b6b1f0194a5
Olayinka-2 <olayinkaisrael802@gmail.com>
2024-10-09 12:23:16 +0100

    feat: Add the remove method to the hash table

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 import { LinkedList } from "./linkedlist.js";
 
 class HashMap {
+   #loadFactor;
+   #initialCapacity;
    constructor(initialCapacity = 16, load = 0.8) {
-      this.initialCapacity = initialCapacity;
-      this.loadFactor = load;
-      this.buckets = new Array(this.initialCapacity).fill(null);
+      this.#initialCapacity = initialCapacity;
+      this.#loadFactor = load;
+      this.buckets = new Array(this.#initialCapacity).fill(null);
       this.size = 0;
    }
    
@@ -22,7 +24,7 @@ class HashMap {
 
    #getBucketIndex(key) {
       let keys = this.#hash(key);
-      return Math.abs(keys % this.initialCapacity);
+      return Math.abs(keys % this.#initialCapacity);
    }
 
 
@@ -47,30 +49,86 @@ class HashMap {
 
       this.size++;
 
-      if(this.size / this.initialCapacity > this.loadFactor) {
+      if(this.size / this.#initialCapacity > this.loadFactor) {
          this.#resize();
       }
    }
 
+   get(key) {
+      for(let element of this.buckets) {
+
+         if(element !== null) {
+            let elementNode = element.head;
+            while(elementNode !== null) {
+      
+               if(elementNode.key === key) {
+                  return elementNode.value;
+               }
+               elementNode = elementNode.next;
+            }
+         }
+
+      }
+      return null;
+   }
+
+   has(key) {
+      for(let element of this.buckets) {
+
+         if(element !== null) {
+            let elementNode = element.head;
+            while(elementNode !== null) {
+      
+               if(elementNode.key === key) {
+                  return true;
+               }
+               elementNode = elementNode.next;
+            }
+         }
+
+      }
+      return false;
+   }
+
+   remove(key) {
+      for(let element of this.buckets) {
+
+         if(element !== null) {
+            let elementNode = element.head;
+            let index = 0;
+            while(elementNode !== null) {
+      
+               if(elementNode.key === key) {
+               element.removeAt(index);
+               return true
+               }
+               index++;
+               elementNode = elementNode.next;
+            }
+         }
+
+      }
+      return false;
+   }
+
    #resize() {
       const oldBuckets = this.buckets;
-      this.initialCapacity *= 2; // Double the capacity
-      this.buckets = Array(this.capacity).fill(null); // Create new bucket array
+      this.#initialCapacity *= 2; // Double the capacity
+      this.buckets = Array(this.#initialCapacity).fill(null); // Create new bucket array
       this.size = 0; // Reset size, we will re-add elements
-  
+
       // Rehash all existing key-value pairs
       for (let bucket of oldBuckets) {
-        if (bucket) {
+         if (bucket) {
 
          let currentNode = bucket.head;
          while(currentNode !== null) {
             this.set(currentNode.key, currentNode.value);
             currentNode = currentNode.next;
          }
-        }
       }
-    }
-  
+      }
+   }
 }
 
 
@@ -93,6 +151,9 @@ man.set('mand', 2);
 man.set('maszn', 1);
 man.set('zmane', 2);
 man.set('cmanse', 1);
-man.set('cmarn', 2);
-console.log(man.buckets);
-console.log(man.size);
+man.set('cmarn', 255);
+// console.log(man.buckets);
+// console.log(man.size);
+
+// console.log(man.has(null));
+console.log(man.remove('cmarn'));
